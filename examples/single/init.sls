5e5e4fd7decee5b10f4d44e8ce7c8e7c73c0ea2e
Martin Walsh <marty@notoptimal.com>
2018-11-12 07:32:44 -0600

    passing tests for a single formula

--- examples/single/init.sls
+++ examples/single/init.sls
@@ -35,9 +35,59 @@ single_confd_entry:
   file.managed:
     - name: /etc/single/conf.d/entry.conf
     - source: salt://{{ slspath }}/templates/entry.conf.j2
+    - template: jinja
     - makedirs: true
     - require_in:
       - file: single_confd_directory
+
+
+single_rename_file:
+  file.rename:
+    - name: /etc/single/renamed.txt
+    - source: /etc/single/rename-me.txt
+
+
+single_append_to_file_with_text:
+  file.append:
+    - name: /etc/single/single.conf
+    - text: |
+        Text to append
+
+
+single_append_to_file_with_source:
+  file.append:
+    - name: /etc/single/single.conf
+    - source: salt://{{ slspath }}/files/append-single.txt
+
+
+single_append_to_file_with_multiple_sources:
+  file.append:
+    - name: /etc/single/single.conf
+    - sources:
+        - salt://{{ slspath }}/files/append-one.txt
+        - salt://{{ slspath }}/files/append-two.txt
+
+
+single_recurce_directory:
+  file.recurse:
+    - name: /etc/single-recurse
+    - source: salt://{{ slspath }}/files/single-recurse
+
+
+single_file_managed_with_multiple_sources_first:
+  file.managed:
+    - name: /etc/single/multisource-pickone.txt
+    - source:
+      - salt://{{ slspath }}/files/this-one-exists.txt
+      - salt://{{ slspath }}/files/this-one-does-not-exist.txt
+
+
+single_file_managed_with_multiple_sources_second:
+  file.managed:
+    - name: /etc/single/multisource-picktwo.txt
+    - source:
+      - salt://{{ slspath }}/files/this-one-does-not-exist.txt
+      - salt://{{ slspath }}/files/this-one-exists.txt
 
 
 single_manage_service:
--- examples/single/test/unit/single/test_single.py
+++ examples/single/test/unit/single/test_single.py
@@ -1,9 +1,67 @@
 from expects import expect
 
 
-def test_contains_single_config(show_low_sls, contain_file, contain_service, contain_pkg):
+def test_contains_install_dependencies(show_low_sls, contain_pkg):
+    with show_low_sls('single', {}) as sls:
+        expect(sls).to(contain_pkg('unzip'))
+        expect(sls).to(contain_pkg('curl'))
+
+
+def test_contains_single_install_package(show_low_sls, contain_pkg):
+    with show_low_sls('single', {}) as sls:
+        expect(sls).to(contain_pkg('htop').with_version('latest'))
+
+
+def test_contains_main_config(show_low_sls, contain_file):
     with show_low_sls('single', {}) as sls:
         expect(sls).to(
             contain_file('/etc/single/single.conf')
             .that_has_content('/etc/single/single.conf')
+        )
+
+
+def test_contains_config_d(show_low_sls, contain_file):
+    with show_low_sls('single', {}) as sls:
+        expect(sls).to(
+            contain_file('/etc/single/conf.d').with_clean(True)
+        )
+
+
+def test_contains_confd_entry(show_low_sls, contain_file):
+    with show_low_sls('single', {}) as sls:
+        expect(sls).to(
+            contain_file('/etc/single/conf.d/entry.conf')
+            .that_has_content('somevalue')
+        )
+
+
+def test_contains_renamed_file(show_low_sls, contain_file):
+    with show_low_sls('single', {}) as sls:
+        expect(sls).to(
+            contain_file('/etc/single/renamed.txt')
+            .with_source('/etc/single/rename-me.txt')
+        )
+
+
+def test_contains_appended_file(show_low_sls, contain_file):
+    with show_low_sls('single', {}) as sls:
+        expect(sls).to(
+            contain_file('/etc/single/single.conf')
+            .with_text('Text to append\n')
+        )
+
+
+def test_contains_appended_file_with_source(show_low_sls, contain_file):
+    with show_low_sls('single', {}) as sls:
+        expect(sls).to(
+            contain_file('/etc/single/single.conf')
+            .that_has_content('append-single.txt')
+        )
+
+
+def test_contains_appended_file_with_multiple_sources(show_low_sls, contain_file):
+    with show_low_sls('single', {}) as sls:
+        expect(sls).to(
+            contain_file('/etc/single/single.conf')
+            .that_has_content('append-one.txt\nappend-two.txt\n')
         )
