c1184f8caa06a53f4127ea95ac75bc6918723ab6
Martin Walsh <marty@notoptimal.com>
2018-11-14 05:50:27 -0600

    complete tests for 'single' example

--- examples/single/test/unit/single/test_single.py
+++ examples/single/test/unit/single/test_single.py
@@ -1,3 +1,4 @@
+import re
 from expects import expect
 
 
@@ -64,4 +65,36 @@ def test_contains_appended_file_with_multiple_sources(show_low_sls, contain_file
         expect(sls).to(
             contain_file('/etc/single/single.conf')
             .that_has_content('append-one.txt\nappend-two.txt\n')
+        )
+
+
+def test_contains_recurse_dir(show_low_sls, contain_file):
+    with show_low_sls('single', {}) as sls:
+        expect(sls).to(
+            contain_file('/etc/single-recurse')
+            .with_source('salt://single/files/single-recurse')
+        )
+
+
+def test_contains_file_from_multiple_sources_first_exists(show_low_sls, contain_id, contain_file):
+    with show_low_sls('single', {}) as sls:
+        expect(sls).to(
+            contain_id('single_file_managed_with_multiple_sources_first')
+            .with_source([
+                'salt://single/files/this-one-exists.txt',
+                'salt://single/files/this-one-does-not-exist.txt',
+            ])
+        )
+        expect(sls).to(
+            contain_file('/etc/single/multisource-pickone.txt')
+            .that_has_content('this-one-exists.txt')
+        )
+
+
+def test_contains_file_from_multiple_sources_second_exists(show_low_sls, contain_file):
+    with show_low_sls('single', {}) as sls:
+        pattern = re.compile(r'^# files\/this-one-exists\.txt$')
+        expect(sls).to(
+            contain_file('/etc/single/multisource-picktwo.txt')
+            .that_has_content(pattern)
         )
